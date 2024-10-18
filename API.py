72612ca4026db01c55a2496626feaa08cf1dcc49
latidoenlinea <latidoenlinea@gmail.com>
2024-10-18 12:47:19 -0600

    API.py

--- API.py
+++ API.py
@@ -133,4 +133,4 @@ def guardar_datos_excel():
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
